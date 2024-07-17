0f4d0eb4e7e2a0cdf55aa7eb0ce61040af00c173
CegoMaeva <maevachoupo@gmail.com>
2024-07-17 18:44:18 -0400

    premier commit

---
+++ trim.js
@@ -0,0 +1,16 @@
+var texte="Bienvenue a tous messi tous maroc";
+document.write(texte.lenght);+"br"
+document.write(texte[0]);+"br"
+document.write(texte[9]);+"br"
+document.write(texte.toUpperCase());+"br"
+document.write(texte.toLowerCase());+"br"
+document.write(texte.trim());
+document.write(texte.indexOf("tous"));+"br"
+var etats=["ohio","missouri","arizona","virginie"];
+document.write(etats+"br");+"br"
+etats.push("Caroline du nord");
+document.write(etats);+"br"
+etats.shift();
+document.write(etats);+"br"
+etats.pop();
+document.write(etats);+"br"
